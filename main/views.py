10e7c7937e47ea385dc85b89f032586638b02c96
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-06-04 14:04:19 +0200

    first commit

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
