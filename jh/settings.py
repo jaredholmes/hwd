2fc62e8f5c17d26a042f5ad058ecfa48cb93d91d
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-06-06 11:11:38 +0200

    Added __str__ method for Message

--- jh/settings.py
+++ jh/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.eviron['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Password validation
