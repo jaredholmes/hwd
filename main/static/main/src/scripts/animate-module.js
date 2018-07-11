7cff62954c3ed95ec68a8a93bc5f1610d8bd1961
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-07-11 15:21:16 +0200

    Changed from vanilla to Vue

--- main/static/main/src/scripts/animate-module.js
+++ main/static/main/src/scripts/animate-module.js
@@ -1,10 +1,28 @@
 import 'animate.css/animate.min.css';
-// import Vue from 'vue';
+import Vue from 'vue';
 
-// var v = new Vue({
-//   el: '#app',
-//   delimiters: ['[[', ']]'],
-//   data: {
-//
-//   }
-// });
+new Vue({
+  el: '#app',
+  delimiters: ['[[', ']]'],
+  data: {
+  },
+  methods: {
+    timeout(i) {
+      var arrText = text;
+      setTimeout( () => {
+        var slogan = document.getElementById('slogan-1');
+        slogan.setAttribute('class', 'animated fadeOutRight');
+        setTimeout( () => {
+          slogan.innerHTML = arrText[i];
+          slogan.setAttribute('class', 'animated fadeInLeft');
+        }, 100);
+        if (i < (arrText.length - 1)) {
+          i++;
+        } else {
+          i = 0;
+        }
+        this.timeout(i);
+      }, 6500);
+    },
+  },
+});
