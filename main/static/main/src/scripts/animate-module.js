9d116a82a0cc76d1142f09d98eeddd7f8131a2c1
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-07-12 08:33:41 +0200

    Added comments

--- main/static/main/src/scripts/animate-module.js
+++ main/static/main/src/scripts/animate-module.js
@@ -4,15 +4,20 @@ import Vue from 'vue';
 
 new Vue({
   el: '#app',
+  // Delimiters prevent clashing with Jinja
   delimiters: ['[[', ']]'],
   data: {
   },
   methods: {
     timeout(i) {
+      // text is declared in HTML to allow {% static %} use with Django
       var arrText = text;
+      // Arrow function allows later reference to this.timeout, since it ensures
+      // that this remains the Vue object
       setTimeout( () => {
         var slogan = document.getElementById('slogan-1');
         slogan.setAttribute('class', 'animated fadeOutRight');
+        // The contents of the slogan change and it is faded back in after 100ms
         setTimeout( () => {
           slogan.innerHTML = arrText[i];
           slogan.setAttribute('class', 'animated fadeInLeft');
@@ -22,6 +27,7 @@ new Vue({
         } else {
           i = 0;
         }
+        // Recall method for recursive loop
         this.timeout(i);
       }, 6500);
     },
